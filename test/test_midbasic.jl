af6c470e8cba7357cd20ceb60cb9db573f7ea3c7
Neel Smith <dnsmith.neel@gmail.com>
2021-07-03 04:28:28 -0400

    Borken start on parsing TM edition

--- test/test_midbasic.jl
+++ test/test_midbasic.jl
@@ -1,5 +1,17 @@
-@testset "Test usage of MID element names" begin
+@testset "Test basic TEI usage defined by MID module" begin
     tmbldr = HmtTopicModels.HmtTMBuilder("tm builder", "x")
     validnames = EditionBuilders.validElementNames(tmbldr)
     @test length(validnames) == 33
+    @test validelname(tmbldr, "p")
+    @test validelname(tmbldr, "watermark") == false
+
+
+    choicexml = "<choice><abbr>Mr.</abbr><expan>Mister</expan></choice>"
+    choicenode = EzXML.parsexml(choicexml) |> root
+    @test EditionBuilders.validchoice(choicenode)
+
+    wxml = "<w>Word<unclear>less</unclear></w>"
+    wnode =  EzXML.parsexml(wxml) |> root
+    @test EditionBuilders.collectw(wnode, tmbldr) == "Wordless"
+
 end
