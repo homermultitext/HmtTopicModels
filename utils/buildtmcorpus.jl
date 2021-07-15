888527a145aeaa5a4aed4e5b24a15c13fbff9f9b
Neel Smith <dnsmith.neel@gmail.com>
2021-07-15 14:48:11 -0400

    Filter empty text out

--- utils/buildtmcorpus.jl
+++ utils/buildtmcorpus.jl
@@ -9,8 +9,8 @@ using CitableCorpus
 tmbldr = HmtTopicModels.HmtTMBuilder("tm builder", "hmttm")
 xmlurl = "https://raw.githubusercontent.com/hmteditors/composite-summer21/main/data/archive-xml.cex"
 xmlcorpus = CitableCorpus.fromurl(CitableTextCorpus, xmlurl, "|")
-scholia = filter(cn -> endswith(cn.urn.urn, "comment"), xmlcorpus.corpus) |> CitableTextCorpus
-
+allscholia = filter(cn -> endswith(cn.urn.urn, "comment"), xmlcorpus.corpus) |> CitableTextCorpus
+scholia = filter(cn -> ! isempty(cn.text), allscholia.corpus) |> CitableTextCorpus
 #using HTTP
 #raw = String(HTTP.get(xmlurl).body)
 #occursin("<persName>", raw)
