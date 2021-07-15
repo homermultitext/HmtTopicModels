faf3693e23490db4b36010d09115a8abda33ed23
Neel Smith <dnsmith.neel@gmail.com>
2021-07-15 14:51:44 -0400

    Filter nonempty from TM edition

--- utils/buildtmcorpus.jl
+++ utils/buildtmcorpus.jl
@@ -9,16 +9,15 @@ using CitableCorpus
 tmbldr = HmtTopicModels.HmtTMBuilder("tm builder", "hmttm")
 xmlurl = "https://raw.githubusercontent.com/hmteditors/composite-summer21/main/data/archive-xml.cex"
 xmlcorpus = CitableCorpus.fromurl(CitableTextCorpus, xmlurl, "|")
-allscholia = filter(cn -> endswith(cn.urn.urn, "comment"), xmlcorpus.corpus) |> CitableTextCorpus
-scholia = filter(cn -> ! isempty(cn.text), allscholia.corpus) |> CitableTextCorpus
+scholia = filter(cn -> endswith(cn.urn.urn, "comment"), xmlcorpus.corpus) |> CitableTextCorpus
+
 #using HTTP
 #raw = String(HTTP.get(xmlurl).body)
 #occursin("<persName>", raw)
 tmedition = edition(tmbldr, scholia)
+nonempty = filter( cn -> ! isempty(strip(cn.text)), tmedition.corpus) |> CitableTextCorpus
 
-
-#tmcex = cex(tmedition)
 tmfile = "topicmodelingedition.cex"
 open(tmfile,"w") do io
-    write(io, cex(tmedition))
+    write(io, cex(nonempty))
 end
